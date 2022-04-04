6f3d660442eb484b2756c99eee0e651d780fb8c6
Morten Six <mort108v@stud.kea.dk>
2022-04-04 11:52:00 +0200

    Table for siteinfo implemented but the fetch of data from API has a problem with mapping it

--- carboncalc.js
+++ carboncalc.js
@@ -3,6 +3,14 @@
 const adviceAPIkey = 'AIzaSyA3T3lU6NyXFlEjJMt739iXmn-GBT_B7qk'
 const inputForm = document.getElementById("siteinput");
 
+const SiteInfo = {
+    myURL: "",
+    timestamp: "",
+    performance: "",
+    overall: "",
+    percentile: "",
+}
+
 inputForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let URLvalue = inputForm.elements.urlname.value;
@@ -18,8 +26,47 @@ function calcMyCarbon(URLvalue) {
         .then(response => {
             console.log(response);
         })
+        .then(data => prepData(data))
         .catch(err => {
             console.error(err);
         });
 
+}
+
+let infos
+
+function prepData(data) {
+    infos = data.map(fillInfo)
+    buildInfoList()
+}
+
+function fillInfo() {
+    const siteInfo = Object.create(SiteInfo)
+
+    siteInfo.myURL = data.id
+    siteInfo.timestamp = data.analysisUTCTimestamp
+    siteInfo.performance = data.performance
+    siteInfo.overall = data.overall_category
+    siteInfo.percentile = data.timing
+
+    console.log(siteInfo)
+    return siteInfo
+}
+
+function buildInfoList() {
+    document.querySelector(".siteinfo tbody.siteinfo__table--tbody").innerHTML = ""
+    infos.forEach(displayInfoList)
+}
+
+function displayInfoList(siteInfo) {
+
+    const siteInfoClone = document.querySelector("template#aboutinfo").content.cloneNode(true)
+
+    siteInfoClone.querySelector("[data-field=timestamp]").textContent = siteInfo.timestamp
+    siteInfoClone.querySelector("[data-field=website]").textContent = siteInfo.myURL
+    siteInfoClone.querySelector("[data-field=performance]").textContent = siteInfo.performance
+    siteInfoClone.querySelector("[data-field=overall]").textContent = siteInfo.overall
+    siteInfoClone.querySelector("[data-field=percentile]").textContent = siteInfo.percentile
+
+    document.querySelector(".siteinfo tbody.siteinfo__table--tbody").appendChild(siteInfoClone)
 }
