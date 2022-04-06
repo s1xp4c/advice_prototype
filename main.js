366ad0b7b0fb77e934db98b988fb2d38ec86d353
Morten Six <mort108v@stud.kea.dk>
2022-04-06 16:36:52 +0200

    Added json and some todo

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 
 import './carboncalc'
 import './rangeslider'
+
+
+// Make divs for lighthouse data
+// Secondary fetch for the co2 thingy
+// Fetch and display the screenshot image
+// Set up restDB
+// Make a dropdown industry menu
+//
