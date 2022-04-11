e00df20866a1fad38a7de930f7870bdf9a159649
Anita Tiwari <anit8621@stud.kea.dk>
2022-04-11 11:01:39 +0200

    scss

--- rangeslider.js
+++ rangeslider.js
@@ -19,3 +19,30 @@ function showSliderValue() {
 
 // Apply scores to a form for database
 // Fetch data from DB and apply to new sliders that can be moved
+// const allRanges = document.querySelectorAll(".range-wrap");
+// allRanges.forEach((wrap) => {
+//   const range = wrap.querySelector(".range");
+//   const bubble = wrap.querySelector(".bubble");
+
+//   range.addEventListener("input", () => {
+//     setBubble(range, bubble);
+//   });
+
+  // setting bubble on DOM load
+//   setBubble(range, bubble);
+// });
+
+function setBubble(range, bubble) {
+  const val = range.value;
+
+  const min = range.min || 0;
+  const max =  range.max || 100;
+  //rangeSlider.value / rangeSlider.max
+
+  const offset = Number(((val - min) * 100) / (max - min));
+
+  bubble.textContent = val;
+
+
+  bubble.style.left = `calc(${offset}% - 14px)`;
+}
